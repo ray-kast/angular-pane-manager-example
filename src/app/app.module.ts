353f4bb3b4cc651ab5faadfd86a58eeb996a72ab
ryans <ryans@opuslogica.com>
2019-07-18 14:12:41 -0700

    Added a simple demo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {NgPaneManager2Module} from 'projects/ng-pane-manager2/src/public-api';
 
 import {AppComponent} from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, NgPaneManager2Module],
     providers: [],
     bootstrap: [AppComponent]
 })
