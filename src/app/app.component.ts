f70edc99d1782fcffd14b43ff661c746558880ba
ryans <ryans@opuslogica.com>
2019-07-30 13:54:42 -0700

    More tabs!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,57 +7,67 @@ import {loadLayout, PaneLayout, saveLayout} from 'projects/ng-pane-manager2/src/
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    paneLayout: PaneLayout = loadLayout({
-        split: 'vert',
-        ratio: [1, 4],
-        children:
-            [
-                {gravity: 'top', id: 'top', template: 'top'},
-                {
-                    split: 'horiz',
-                    ratio: [1, 3, 1],
-                    children:
-                        [
-                            {gravity: 'left', id: 'left', template: 'sideLeft'},
-                            {
-                                gravity: 'center',
-                                split: 'vert',
-                                ratio: [2, 1],
-                                children:
-                                    [
-                                        {id: 'main', template: 'main'},
-                                        {
-                                            gravity: 'bottom',
-                                            split: 'tab',
-                                            currentTab: 0,
-                                            children:
-                                                [
-                                                    {id: 'bottom1', template: 'bottom1'},
-                                                    {id: 'bottom2', template: 'bottom2'},
-                                                    {
-                                                        split: 'horiz',
-                                                        ratio: [1, 1],
-                                                        children:
-                                                            [
-                                                                {
-                                                                    id: 'bottom3l',
-                                                                    template: 'bottom3'
-                                                                },
-                                                                {
-                                                                    id: 'bottom3r',
-                                                                    template: 'bottom1'
-                                                                },
-                                                            ],
-                                                    },
-                                                ],
-                                        },
-                                    ],
-                            },
-                            {gravity: 'right', id: 'right', template: 'sideRight'},
-                        ],
-                },
-            ],
-    });
+    paneLayout:
+        PaneLayout = loadLayout(
+            {
+                split: 'vert',
+                ratio: [1, 4],
+                children:
+                    [
+                        {gravity: 'top', id: 'top', template: 'top'},
+                        {
+                            split: 'horiz',
+                            ratio: [1, 3, 1],
+                            children:
+                                [
+                                    {gravity: 'left', id: 'left', template: 'sideLeft'},
+                                    {
+                                        split: 'vert',
+                                        ratio: [2, 1],
+                                        children:
+                                            [
+                                                {
+                                                    gravity: 'center',
+                                                    split: 'tab',
+                                                    currentTab: 0, 
+                                                    children:
+                                                        [
+                                                            {id: 'main1', template: 'main1'},
+                                                            {id: 'main2', template: 'main2'},
+                                                        ],
+                                                },
+                                                {
+                                                    gravity: 'bottom',
+                                                    split: 'tab',
+                                                    currentTab: 0,
+                                                    children:
+                                                        [
+                                                            {id: 'bottom1', template: 'bottom1'},
+                                                            {id: 'bottom2', template: 'bottom2'},
+                                                            {
+                                                                split: 'horiz',
+                                                                ratio: [1, 1],
+                                                                children:
+                                                                    [
+                                                                        {
+                                                                            id: 'bottom3l',
+                                                                            template: 'bottom3'
+                                                                        },
+                                                                        {
+                                                                            id: 'bottom3r',
+                                                                            template: 'bottom1'
+                                                                        },
+                                                                    ],
+                                                            },
+                                                        ],
+                                                },
+                                            ],
+                                    },
+                                    {gravity: 'right', id: 'right', template: 'sideRight'},
+                                ],
+                        },
+                    ],
+            });
 
     ngOnInit() {
         console.log(saveLayout(this.paneLayout));
