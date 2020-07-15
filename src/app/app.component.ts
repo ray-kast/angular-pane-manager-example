3799b2fba9874f15854c1fb108d4717f4bd5b758
rookie1024 <rookie1286@gmail.com>
2020-07-14 19:29:09 -0700

    Started dragon drop, changed linter rules.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
+// tslint:disable no-implicit-dependencies no-magic-numbers
 import {Component} from '@angular/core';
 import {
     headerStyle,
     loadLayout,
+    PaneHeaderStyle,
     RootLayout,
     StringHeaderMode,
 } from 'projects/angular-pane-manager/src/public-api';
 
+/** Root component of the app */
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -13,8 +16,8 @@ import {
 })
 export class AppComponent {
     // TODO: add a tagged template for making writing layouts easier?
-    // TODO: fix completed-docs tslint rule
-    paneLayout: RootLayout =
+    /** Layout of the main pane manager */
+    public paneLayout: RootLayout =
         loadLayout({
             split: 'vert',
             ratio: [1, 4],
@@ -75,7 +78,61 @@ export class AppComponent {
                 ],
         }).intoRoot();
 
-    protected header(mode: StringHeaderMode, title: string, icon: string, closable: boolean) {
+    /** Add headerStyle to the template context */
+    protected header(mode: StringHeaderMode, title: string, icon: string, closable: boolean):
+        PaneHeaderStyle {
         return headerStyle(mode, title, icon, closable);
+    }
+
+    /** Helper function to test re-rendering the layout when it changes */
+    protected changeLayout(): void {
+        this.paneLayout =
+            loadLayout({
+                split: 'vert',
+                ratio: [1, 5],
+                children: [
+                    {gravity: 'top', id: 'top', template: 'top'},
+                    {
+                        split: 'horiz',
+                        ratio: [1, 3, 1],
+                        children: [
+                            {gravity: 'left', id: 'left', template: 'sideLeft'},
+                            {
+                                split: 'vert',
+                                ratio: [2, 1],
+                                children: [
+                                    {
+                                        gravity: 'center',
+                                        split: 'tab',
+                                        currentTab: 0,
+                                        children: [
+                                            {id: 'main1', template: 'main1'},
+                                            {id: 'main2', template: 'main2'},
+                                        ],
+                                    },
+                                    {
+                                        gravity: 'bottom',
+                                        split: 'tab',
+                                        currentTab: 0,
+                                        children: [
+                                            {id: 'bottom1', template: 'bottom1'},
+                                            {id: 'bottom2', template: 'bottom2'},
+                                            {
+                                                split: 'horiz',
+                                                ratio: [1, 1],
+                                                children: [
+                                                    {id: 'bottom3l', template: 'bottom3'},
+                                                    {id: 'bottom3r', template: 'bottom1'},
+                                                ],
+                                            },
+                                        ],
+                                    },
+                                ],
+                            },
+                            {gravity: 'right', id: 'right', template: 'sideRight'},
+                        ],
+                    },
+                ],
+            }).intoRoot();
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+// tslint:disable no-implicit-dependencies
+
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AngularPaneManagerModule} from 'projects/angular-pane-manager/src/public-api';
 
 import {AppComponent} from './app.component';
 
+/** The root module of the application */
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, AngularPaneManagerModule],
