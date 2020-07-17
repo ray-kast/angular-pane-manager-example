ab09ef13f3f65e0aed718822ce021c54e2418097
rookie1024 <rookie1286@gmail.com>
2020-07-16 21:18:50 -0700

    Extras!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,66 +17,98 @@ import {
 export class AppComponent {
     // TODO: add a tagged template for making writing layouts easier?
     /** Layout of the main pane manager */
-    public paneLayout: RootLayout =
-        loadLayout({
-            split: 'vert',
-            ratio: [1, 4],
-            children:
-                [
-                    {gravity: 'top', id: 'top', template: 'top'},
-                    {
-                        split: 'horiz',
-                        ratio: [1, 3, 1],
-                        children:
-                            [
-                                {gravity: 'left', id: 'left', template: 'sideLeft'},
-                                {
-                                    split: 'vert',
-                                    ratio: [2, 1],
-                                    children:
-                                        [
-                                            {
-                                                gravity: 'center',
-                                                split: 'horiz',
-                                                ratio: [1, 1],
-                                                children:
-                                                    [
-                                                        {id: 'main1', template: 'main1'},
-                                                        {id: 'main2', template: 'main2'},
-                                                    ],
-                                            },
-                                            {
-                                                gravity: 'bottom',
-                                                split: 'tab',
-                                                currentTab: 0,
-                                                children:
-                                                    [
-                                                        {id: 'bottom1', template: 'bottom1'},
-                                                        {id: 'bottom2', template: 'bottom2'},
-                                                        {
-                                                            split: 'horiz',
-                                                            ratio: [1, 1],
-                                                            children:
-                                                                [
-                                                                    {
-                                                                        id: 'bottom3l',
-                                                                        template: 'bottom3',
-                                                                    },
-                                                                    {
-                                                                        id: 'bottom3r',
-                                                                        template: 'bottom1',
-                                                                    },
-                                                                ],
-                                                        },
-                                                    ],
-                                            },
-                                        ],
-                                },
-                                {gravity: 'right', id: 'right', template: 'sideRight'},
-                            ],
-                    },
-                ],
-        }).intoRoot();
+    public paneLayout:
+        RootLayout<string> =
+            loadLayout(
+                {
+                    split: 'vert',
+                    ratio: [1, 4],
+                    children:
+                        [
+                            {gravity: 'top', id: 'top', template: 'top', extra: 'top'},
+                            {
+                                split: 'horiz',
+                                ratio: [1, 3, 1],
+                                children:
+                                    [
+                                        {
+                                            gravity: 'left',
+                                            id: 'left',
+                                            template: 'sideLeft',
+                                            extra: 'left',
+                                        },
+                                        {
+                                            split: 'vert',
+                                            ratio: [2, 1],
+                                            children:
+                                                [
+                                                    {
+                                                        gravity: 'center',
+                                                        split: 'horiz',
+                                                        ratio: [1, 1],
+                                                        children:
+                                                            [
+                                                                {
+                                                                    id: 'main1',
+                                                                    template: 'main1',
+                                                                    extra: 'main1',
+                                                                },
+                                                                {
+                                                                    id: 'main2',
+                                                                    template: 'main2',
+                                                                    extra: 'main2',
+                                                                },
+                                                            ],
+                                                    },
+                                                    {
+                                                        gravity: 'bottom',
+                                                        split: 'tab',
+                                                        currentTab: 0,
+                                                        children:
+                                                            [
+                                                                {
+                                                                    id: 'bottom1',
+                                                                    template: 'bottom1',
+                                                                    extra: 'bottom1',
+                                                                },
+                                                                {
+                                                                    id: 'bottom2',
+                                                                    template: 'bottom2',
+                                                                    extra: 'bottom2',
+                                                                },
+                                                                {
+                                                                    split: 'horiz',
+                                                                    ratio: [1, 1],
+                                                                    children:
+                                                                        [
+                                                                            {
+                                                                                id: 'bottom3l',
+                                                                                template: 'bottom3',
+                                                                                extra: 'bottom3l',
+                                                                            },
+                                                                            {
+                                                                                id: 'bottom3r',
+                                                                                template: 'bottom1',
+                                                                                extra: 'bottom3r',
+                                                                            },
+                                                                        ],
+                                                                },
+                                                            ],
+                                                    },
+                                                ],
+                                        },
+                                        {
+                                            gravity: 'right',
+                                            id: 'right',
+                                            template: 'sideRight',
+                                            extra: 'right',
+                                        },
+                                    ],
+                            },
+                        ],
+                },
+                x => x)
+                .intoRoot();
 
     /** Add headerStyle to the template context */
     public header(mode: StringHeaderMode, title: string, icon: string, closable: boolean):
@@ -91,12 +123,12 @@ export class AppComponent {
                 split: 'vert',
                 ratio: [1, 5],
                 children: [
-                    {gravity: 'top', id: 'top', template: 'top'},
+                    {gravity: 'top', id: 'top', template: 'top', extra: 'TOP'},
                     {
                         split: 'horiz',
                         ratio: [1, 3, 1],
                         children: [
-                            {gravity: 'left', id: 'left', template: 'sideLeft'},
+                            {gravity: 'left', id: 'left', template: 'sideLeft', extra: 'LEFT'},
                             {
                                 split: 'vert',
                                 ratio: [2, 1],
@@ -106,8 +138,8 @@ export class AppComponent {
                                         split: 'tab',
                                         currentTab: 0,
                                         children: [
-                                            {id: 'main1', template: 'main1'},
-                                            {id: 'main2', template: 'main2'},
+                                            {id: 'main1', template: 'main1', extra: 'MAIN1'},
+                                            {id: 'main2', template: 'main2', extra: 'MAIN2'},
                                         ],
                                     },
                                     {
@@ -115,24 +147,39 @@ export class AppComponent {
                                         split: 'tab',
                                         currentTab: 0,
                                         children: [
-                                            {id: 'bottom1', template: 'bottom1'},
-                                            {id: 'bottom2', template: 'bottom2'},
+                                            {id: 'bottom1', template: 'bottom1', extra: 'BOTTOM1'},
+                                            {id: 'bottom2', template: 'bottom2', extra: 'BOTTOM2'},
                                             {
                                                 split: 'horiz',
                                                 ratio: [1, 1],
                                                 children: [
-                                                    {id: 'bottom3l', template: 'bottom3'},
-                                                    {id: 'bottom3r', template: 'bottom1'},
+                                                    {
+                                                        id: 'bottom3l',
+                                                        template: 'bottom3',
+                                                        extra: 'BOTTOM3L',
+                                                    },
+                                                    {
+                                                        id: 'bottom3r',
+                                                        template: 'bottom1',
+                                                        extra: 'BOTTOM3R',
+                                                    },
                                                 ],
                                             },
                                         ],
                                     },
                                 ],
                             },
-                            {gravity: 'right', id: 'right', template: 'sideRight'},
+                            {
+                                gravity: 'right',
+                                id: 'right',
+                                template: 'sideRight',
+                                extra: 'RIGHT',
+                            },
                         ],
                     },
                 ],
-            }).intoRoot();
+            },
+                       x => x)
+                .intoRoot();
     }
 }
